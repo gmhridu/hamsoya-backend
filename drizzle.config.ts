fe6b07c336226f2f91e5eaac2c5299567c9c634a
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2025-10-17 23:53:01 +0600

    refactor(db): update dotenv configuration in database files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
-import "dotenv/config";
-
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
+config({ path: ".env" });
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,11 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL environment variable is required");
-}
+config({ path: ".env" });
 
-const sql = neon(databaseUrl);
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql, schema });
 
 // export const db = drizzle(process.env.DATABASE_URL!, { schema });
