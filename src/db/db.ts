b7f3204b857da891a9716aa3b9d1df70b1bfe8be
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2025-10-17 23:45:41 +0600

    refactor(db): remove dotenv config and make DATABASE_URL optional

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,11 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
 
-config({ path: ".env" });
-
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL || "");
 export const db = drizzle({ client: sql, schema });
 
 // export const db = drizzle(process.env.DATABASE_URL!, { schema });
